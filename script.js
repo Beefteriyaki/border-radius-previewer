0bafd4ca444cc5305584c555050f3809182cec1a
Alex David <ad26creations@gmail.com>
2022-03-15 10:37:15 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let topLeft = document.querySelector('.rectangle').style.border-top-left-radius;document.querySelector('.rectangle').style.border-top-left-radius;
+let topRight = document.querySelector('.rectangle').style.border-top-left-radius;document.querySelector('.rectangle').style.border-top-right-radius;
+let bottomLeft = document.querySelector('.rectangle').style.border-bottom-left-radius;document.querySelector('.rectangle').style.border-top-left-radius;
+let bottomRight = document.querySelector('.rectangle').style.border-top-left-radius;document.querySelector('.rectangle').style.border-bottom-right-radius;
+
